5c9a00cbf107dece2a8f200dfd37815e5165e2c9
Pourya <pourya.nejadensan@gmail.com>
2024-07-15 11:21:11 +0200

    Step 5: Write a "Main" class where you apply some examples of creating Students, Teachers, and Courses using the constructors.

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Student pourya = new Student(1,"pourya","Hamburg",1);
+        Teacher teacher = new Teacher(1,"teacher","Informatik");
+        Course informatik = new Course(1,"Informatik",teacher,pourya);
+
+        System.out.println(pourya);
+        System.out.println(teacher);
+        System.out.println(informatik);
+    }
+}
